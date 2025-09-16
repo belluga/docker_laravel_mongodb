19f694a67bc1eda37b95b4311c4367a902f204bb
Elton Morais <elton@bellugasolutions.com.br>
2025-09-15 21:42:56 -0300

    working storage and flutter

--- docker/laravel-app/entrypoint.sh
+++ docker/laravel-app/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-set -e # Exit immediately if a command exits with a non-zero status.
+set -e
 
-# If vendor directory doesn't exist, run composer install
 if [ ! -f "vendor/autoload.php" ]; then
     echo ">>> Installing Laravel dependencies..."
     composer install --no-interaction --prefer-dist --optimize-autoloader
 fi
 
-# If .env file doesn't exist, copy it and generate the key
 if [ ! -f ".env" ]; then
     cp .env.example .env
     php artisan key:generate
     php artisan config:cache
 fi
 
-# Execute the main command passed from docker-compose (php-fpm)
+if [ ! -L "public/storage" ]; then
+    echo ">>> Creating storage symlink..."
+    php artisan storage:link --relative
+fi
+
 exec "$@"
